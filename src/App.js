26b786519c6b14f558c5b262867be03b8252d27d
vlad05x <organizm777alf@gmail.com>
2023-12-19 17:11:57 +0200

    btn-fix 2.0

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function Result({ correct }) {
       <h2>
         Вы отгадали {correct} ответа из {questions.length}
       </h2>
-      <a href="#">
+      <a href="/">
         <button>Попробовать снова</button>
       </a>
     </div>
