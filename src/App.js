9752f3f00df48c29cb5ed7e1f8a6c8bfd8a29dcd
vlad05x <organizm777alf@gmail.com>
2023-12-19 20:14:15 +0200

    fix-bag

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function Result({ correct }) {
         alt="Result Icon"
       />
       <h2>
-        Вы отгадали {correct} ответов из {questions.length}
+        Вы отгадали {correct} ответо из {questions.length}
       </h2>
       <a href="/Quize">
         <button>Попробовать снова</button>
